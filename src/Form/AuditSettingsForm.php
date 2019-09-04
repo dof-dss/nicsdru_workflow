a666044ccfeacc57492f7b0a269d9d1c22ee541e
DuttonMa <martin@theduttons.org>
2019-09-04 17:11:42 +0100

    auto create fields

--- src/Form/AuditSettingsForm.php
+++ src/Form/AuditSettingsForm.php
@@ -4,6 +4,8 @@ namespace Drupal\nicsdru_workflow\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\node\Entity\NodeType;
 
 /**
@@ -81,12 +83,57 @@ class AuditSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
+    // Check to see if any new content types have been selected.
+    // TODO see if any removed !
+    $config = $this->config('nicsdru_workflow.auditsettings');
+    $old_content_type_list = $config->get('audit_content_types');
+    $new_content_type_list = $form_state->getValue('audit_content_types');
+    foreach($new_content_type_list as $this_type) {
+      if (!$this_type) continue;
+      if (!$old_content_type_list[$this_type]) {
+        $this->addAuditField($this_type);
+      }
+    }
+
     $this->config('nicsdru_workflow.auditsettings')
       ->set('audit_button_text', $form_state->getValue('audit_button_text'))
       ->set('audit_button_hover_text', $form_state->getValue('audit_button_hover_text'))
       ->set('audit_confirmation_text', $form_state->getValue('audit_confirmation_text'))
       ->set('audit_content_types', $form_state->getValue('audit_content_types'))
       ->save();
+  }
+
+  public function addAuditField($type) {
+    // Add an audit field to the content type.
+    $field_storage = FieldStorageConfig::loadByName('node', 'field_next_audit_due');
+    $field = FieldConfig::loadByName('node', $type, 'field_next_audit_due');
+    if (empty($field)) {
+      $field = FieldConfig::create([
+        'field_storage' => $field_storage,
+        'bundle' => $type,
+        'label' => 'Next audit due',
+        'settings' => ['display_summary' => TRUE],
+        'description' => t('The date when this item is due for audit'),
+      ]);
+      $field->save();
+
+      $display_repository = \Drupal::service('entity_display.repository');
+
+      // Assign widget settings for the default form mode.
+      $display_repository->getFormDisplay('node', $type)
+        ->setComponent('field_next_audit_due', [
+          'type' => 'datetime_default',
+        ])
+        ->save();
+
+      // Assign display settings for the 'default' and 'teaser' view mode.
+      $display_repository->getViewDisplay('node', $type)
+        ->setComponent('field_next_audit_due', [
+          'label' => 'hidden',
+          'type' => 'text_default',
+        ])
+        ->save();
+    }
   }
 
 }
